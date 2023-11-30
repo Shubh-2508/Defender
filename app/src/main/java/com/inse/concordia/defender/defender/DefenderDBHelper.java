6bac072aeb2848badd375ff3b385d1aff4bf58ae
Shubh <shubh2508@outlook.com>
2023-11-30 02:49:48 -0500

    Defender full app commit ---- - Package data is not collected - Only Process and Event tables are getting populates

--- app/src/main/java/com/inse/concordia/defender/defender/DefenderDBHelper.java
+++ app/src/main/java/com/inse/concordia/defender/defender/DefenderDBHelper.java
@@ -1,6 +1,8 @@
 package com.inse.concordia.defender.defender;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 import android.content.ContentValues;
@@ -17,6 +19,8 @@ import com.inse.concordia.defender.model.MemoryUsage;
 import com.inse.concordia.defender.model.NetworkUsage;
 import com.inse.concordia.defender.model.CPUUsage;
 import com.inse.concordia.defender.model.APackage;
+import com.inse.concordia.defender.model.Alert;
+import com.inse.concordia.defender.model.IEModel;
 
 public class DefenderDBHelper extends SQLiteOpenHelper {
     private final String TAG = DefenderDBHelper.class.getName();
@@ -126,10 +130,43 @@ public class DefenderDBHelper extends SQLiteOpenHelper {
             + " int, " + PACKAGE_NUMERIC_THREAT + " REAL, " + PROCESS_NAME
             + " TEXT, " + PROCESS_UID + " TEXT" + ");";
 
+    // TODO better description
+    // table for IEModel, to hold cumulative resources
+    // fromts, tots, processname, lowcpu, midcpu, highcpu
+    private static final String IEMODEL_TABLE_NAME = "iemodel";
+    private static final String IEMODEL_FROM_TS = "fromts";
+    private static final String IEMODEL_TO_TS = "tots";
+    private static final String IEMODEL_AGE = "age";
+    private static final String IEMODEL_CPU_LOW = "cpu_low";
+    private static final String IEMODEL_CPU_MID = "cpu_mid";
+    private static final String IEMODEL_CPU_HIGH = "cpu_high";
+    private static final String IEMODEL_CPU_COUNTER = "cpu_counter";
 
+    private static final String IEMODEL_TABLE_CREATE = "CREATE TABLE "
+            + IEMODEL_TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY,"
+            + IEMODEL_FROM_TS + " int," + IEMODEL_TO_TS + " int, "
+            + PROCESS_NAME + " TEXT, " + IEMODEL_CPU_LOW + " int, "
+            + IEMODEL_CPU_MID + " int, " + IEMODEL_CPU_HIGH + " int, "
+            + IEMODEL_CPU_COUNTER + " int, " + IEMODEL_AGE + " int, "
+            + NETWORKUSAGE_RX_BYTES + " int, " + NETWORKUSAGE_TX_BYTES + " int"
+            + ");";
+
+    // threat alerts table
+    private static final String THREATALERT_TABLE_NAME = "alert";
+    private static final String THREATALERT_NOTES = "notes";
+    private static final String THREATALERT_TABLE_CREATE = "CREATE TABLE "
+            + THREATALERT_TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY,"
+            + TIMESTAMP + " long," + THREATALERT_NOTES + " TEXT " + ");";
+
+    private static final String LOG_TABLE_NAME = "log";
+    private static final String LOG_DESC = "desc";
+
+    private static final String LOG_TABLE_CREATE = "CREATE TABLE "
+            + LOG_TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY," + TIMESTAMP
+            + " long" + LOG_DESC + "TEXT " + ");";
     private static final String[] tables = new String[] { PROCESS_TABLE_NAME,
-            EVENT_TABLE_NAME, PACKAGE_TABLE_NAME, NETWORKUSAGE_TABLE_NAME,
-            MEMORYUSAGE_TABLE_NAME};
+            CPUUSAGE_TABLE_NAME, EVENT_TABLE_NAME, PACKAGE_TABLE_NAME, NETWORKUSAGE_TABLE_NAME,
+            MEMORYUSAGE_TABLE_NAME, IEMODEL_TABLE_NAME, THREATALERT_TABLE_NAME, LOG_TABLE_NAME};
 
     private DefenderDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -138,11 +175,14 @@ public class DefenderDBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(PACKAGE_TABLE_CREATE);
+        db.execSQL(PROCESS_TABLE_CREATE);
         db.execSQL(CPUUSAGE_TABLE_CREATE);
         db.execSQL(NETWORKUSAGE_TABLE_CREATE);
         db.execSQL(MEMORYUSAGE_TABLE_CREATE);
-        db.execSQL(PROCESS_TABLE_CREATE);
         db.execSQL(EVENT_TABLE_CREATE);
+        db.execSQL(IEMODEL_TABLE_CREATE);
+        db.execSQL(THREATALERT_TABLE_CREATE);
+        db.execSQL(LOG_TABLE_CREATE);
     }
 
     @Override
@@ -364,6 +404,54 @@ public class DefenderDBHelper extends SQLiteOpenHelper {
         return true;
     }
 
+    public boolean insertAlert(Alert al) {
+        SQLiteDatabase aidsDB = this.getWritableDatabase();
+
+        long insertedID = 0;
+        ContentValues values = new ContentValues();
+
+        values.put(TIMESTAMP, al.TimeStamp);
+        values.put(THREATALERT_NOTES, al.Notes);
+
+        try {
+            aidsDB.beginTransaction();
+            insertedID = aidsDB.insert(THREATALERT_TABLE_NAME, null, values);
+            aidsDB.setTransactionSuccessful();
+        } finally {
+            aidsDB.endTransaction();
+        }
+
+        if (insertedID == -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean insertLog(String logText) {
+        SQLiteDatabase aidsDB = this.getWritableDatabase();
+
+        long insertedID = 0;
+        ContentValues values = new ContentValues();
+
+        values.put(TIMESTAMP, Calendar.getInstance().getTimeInMillis());
+        values.put(LOG_DESC, logText);
+
+        try {
+            aidsDB.beginTransaction();
+            insertedID = aidsDB.insert(LOG_TABLE_NAME, null, values);
+            aidsDB.setTransactionSuccessful();
+        } finally {
+            aidsDB.endTransaction();
+        }
+
+        if (insertedID == -1) {
+            return false;
+        }
+
+        return true;
+    }
+
 
 
 
@@ -391,6 +479,326 @@ public class DefenderDBHelper extends SQLiteOpenHelper {
         }
 
         return pList;
+    }
+
+    public List<NetworkUsage> getNetworkUsage(long fromTS, long toTS) {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+        ArrayList<NetworkUsage> nuList = new ArrayList<NetworkUsage>();
+
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(
+                NETWORKUSAGE_TABLE_NAME, new String[] { PROCESS_UID,
+                        NETWORKUSAGE_RX_BYTES, NETWORKUSAGE_TX_BYTES },
+                TIMESTAMP + " between ? and ?",
+                new String[] { String.valueOf(fromTS), String.valueOf(toTS) },
+                null, null, null, null);
+
+        if (cursor.getCount() == 0) {
+            return nuList;
+        }
+
+        while (cursor.moveToNext()) {
+            NetworkUsage nu = new NetworkUsage();
+
+            nu.Uid = cursor.getString(0);
+            nu.RxBytes = cursor.getLong(1);
+            nu.TxBytes = cursor.getLong(2);
+
+            nuList.add(nu);
+        }
+
+        return nuList;
+    }
+
+    /*
+     * Gets recv'd and tx'd bytes for uid and time period. This returns a
+     * hashmap of rx, tx values.
+     */
+    public HashMap<String, String> getBandwidthUsage(String uid, long fromTS,
+                                                     long toTS) {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+        HashMap<String, String> netUse = new HashMap<String, String>();
+
+        netUse.put("rx", "0");
+        netUse.put("tx", "0");
+        // TODO optimize and consider that during this time period, a reset
+        // might have happened
+        // get last record for uid by id order
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(
+                NETWORKUSAGE_TABLE_NAME, new String[] { NETWORKUSAGE_RX_BYTES,
+                        NETWORKUSAGE_TX_BYTES }, TIMESTAMP
+                        + " between ? and ? and " + PROCESS_UID + "=?",
+                new String[] { String.valueOf(fromTS), String.valueOf(toTS),
+                        uid }, null, null, "id desc", "1");
+
+        if (cursor.getCount() == 0) {
+            return netUse;
+        }
+
+        cursor.moveToFirst();
+        int maxRx = cursor.getInt(0);
+        int maxTx = cursor.getInt(1);
+
+        // get first record for uid by id order
+        cursor = (SQLiteCursor) aidsDB.query(NETWORKUSAGE_TABLE_NAME,
+                new String[] { NETWORKUSAGE_RX_BYTES, NETWORKUSAGE_TX_BYTES },
+                TIMESTAMP + " between ? and ? and " + PROCESS_UID + "=?",
+                new String[] { String.valueOf(fromTS), String.valueOf(toTS),
+                        uid }, null, null, "id asc", "1");
+
+        cursor.moveToFirst();
+        int minRx = cursor.getInt(0);
+        int minTx = cursor.getInt(1);
+
+        cursor.close();
+
+        netUse.put("rx", String.valueOf(maxRx - minRx));
+        netUse.put("tx", String.valueOf(maxTx - minTx));
+
+        return netUse;
+    }
+
+    public List<CPUUsage> getCPUUsage(String pid, long fromTS, long toTS) {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+        ArrayList<CPUUsage> cpuList = new ArrayList<CPUUsage>();
+
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(CPUUSAGE_TABLE_NAME,
+                new String[] { CPUUSAGE_CPU }, TIMESTAMP
+                        + " between ? and ? and " + PROCESS_PID + "=?",
+                new String[] { String.valueOf(fromTS), String.valueOf(toTS),
+                        pid }, null, null, null);
+
+        if (cursor.getCount() == 0) {
+            return cpuList;
+        }
+
+        while (cursor.moveToNext()) {
+            CPUUsage cpu = new CPUUsage();
+            cpu.CPUUsage = cursor.getString(0);
+
+            cpuList.add(cpu);
+        }
+
+        cursor.close();
+
+        return cpuList;
+    }
+
+    // get IEModel for process
+    public IEModel getIEModel(String pName) {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(IEMODEL_TABLE_NAME,
+                new String[] { IEMODEL_FROM_TS, IEMODEL_TO_TS, IEMODEL_CPU_LOW,
+                        IEMODEL_CPU_MID, IEMODEL_CPU_HIGH, ID,
+                        IEMODEL_CPU_COUNTER, IEMODEL_AGE,
+                        NETWORKUSAGE_RX_BYTES, NETWORKUSAGE_TX_BYTES },
+                PROCESS_NAME + "=?", new String[] { pName }, null, null, null);
+
+        if (cursor.getCount() == 0) {
+            return null;
+        }
+
+        cursor.moveToFirst(); // only one model for process is expected
+
+        IEModel iem = new IEModel();
+        iem.ProcessName = pName;
+        iem.FromTimeStamp = cursor.getLong(0);
+        iem.ToTimeStamp = cursor.getLong(1);
+        iem.CPULow = cursor.getInt(2);
+        iem.CPUMid = cursor.getInt(3);
+        iem.CPUHigh = cursor.getInt(4);
+        iem.ID = cursor.getInt(5);
+        iem.CPUCounter = cursor.getInt(6);
+        iem.Age = cursor.getInt(7);
+        iem.RxBytes = cursor.getInt(8);
+        iem.TxBytes = cursor.getInt(9);
+
+        cursor.close();
+
+        return iem;
+    }
+
+    // get all IEModels
+    public List<IEModel> getIEModel() {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+        ArrayList<IEModel> ieModelList = new ArrayList<IEModel>();
+
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(IEMODEL_TABLE_NAME,
+                new String[] { IEMODEL_FROM_TS, IEMODEL_TO_TS, IEMODEL_CPU_LOW,
+                        IEMODEL_CPU_MID, IEMODEL_CPU_HIGH, ID,
+                        IEMODEL_CPU_COUNTER, PROCESS_NAME, IEMODEL_AGE,
+                        NETWORKUSAGE_RX_BYTES, NETWORKUSAGE_TX_BYTES }, null,
+                null, null, null, null);
+
+        if (cursor.getCount() == 0) {
+            return ieModelList;
+        }
+
+        while (cursor.moveToNext()) {
+            IEModel iem = new IEModel();
+            iem.FromTimeStamp = cursor.getLong(0);
+            iem.ToTimeStamp = cursor.getLong(1);
+            iem.CPULow = cursor.getInt(2);
+            iem.CPUMid = cursor.getInt(3);
+            iem.CPUHigh = cursor.getInt(4);
+            iem.ID = cursor.getInt(5);
+            iem.CPUCounter = cursor.getInt(6);
+            iem.ProcessName = cursor.getString(7);
+            iem.Age = cursor.getInt(8);
+            iem.RxBytes = cursor.getInt(9);
+            iem.TxBytes = cursor.getInt(10);
+
+            ieModelList.add(iem);
+        }
+
+        cursor.close();
+
+        return ieModelList;
+    }
+
+    public boolean updateIEModel(IEModel iem) {
+        SQLiteDatabase aidsDB = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(IEMODEL_FROM_TS, iem.FromTimeStamp);
+        values.put(IEMODEL_TO_TS, iem.ToTimeStamp);
+        values.put(PROCESS_NAME, iem.ProcessName);
+        values.put(IEMODEL_CPU_LOW, iem.CPULow);
+        values.put(IEMODEL_CPU_MID, iem.CPUMid);
+        values.put(IEMODEL_CPU_HIGH, iem.CPUHigh);
+        values.put(IEMODEL_CPU_COUNTER, iem.CPUCounter);
+        values.put(IEMODEL_AGE, iem.Age);
+        values.put(NETWORKUSAGE_RX_BYTES, iem.RxBytes);
+        values.put(NETWORKUSAGE_TX_BYTES, iem.TxBytes);
+
+        int affectedRows = aidsDB.update(IEMODEL_TABLE_NAME, values, ID + "=?",
+                new String[] { String.valueOf(iem.ID) });
+
+        if (affectedRows != 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean insertIEModel(IEModel iem) {
+        SQLiteDatabase aidsDB = this.getWritableDatabase();
+
+        long insertedID = 0;
+        ContentValues values = new ContentValues();
+
+        values.put(IEMODEL_FROM_TS, iem.FromTimeStamp);
+        values.put(IEMODEL_TO_TS, iem.ToTimeStamp);
+        values.put(PROCESS_NAME, iem.ProcessName);
+        values.put(IEMODEL_CPU_LOW, iem.CPULow);
+        values.put(IEMODEL_CPU_MID, iem.CPUMid);
+        values.put(IEMODEL_CPU_HIGH, iem.CPUHigh);
+        values.put(IEMODEL_CPU_COUNTER, iem.CPUCounter);
+        values.put(IEMODEL_AGE, iem.Age);
+        values.put(NETWORKUSAGE_RX_BYTES, iem.RxBytes);
+        values.put(NETWORKUSAGE_TX_BYTES, iem.TxBytes);
+
+        try {
+            aidsDB.beginTransaction();
+            insertedID = aidsDB.insert(IEMODEL_TABLE_NAME, null, values);
+            aidsDB.setTransactionSuccessful();
+        } finally {
+            aidsDB.endTransaction();
+        }
+
+        if (insertedID == -1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // TODO what to do with the different timestamps?
+    // returns listing of all packages
+    public List<APackage> getPackage() {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+        ArrayList<APackage> pkgList = new ArrayList<APackage>();
+
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(PACKAGE_TABLE_NAME,
+                new String[] { PACKAGE_NAME, PACKAGE_INSTALL_TIME,
+                        PACKAGE_UPDATE_TIME, PACKAGE_NUMERIC_THREAT,
+                        PROCESS_NAME, PROCESS_UID, ID }, null, null, null,
+                null, TIMESTAMP + " DESC");
+
+        if (cursor.getCount() == 0) {
+            return pkgList;
+        }
+
+        while (cursor.moveToNext()) {
+            APackage pkg = new APackage();
+
+            pkg.Name = cursor.getString(0);
+            pkg.InstallTime = cursor.getLong(1);
+            pkg.UpdateTime = cursor.getLong(2);
+            pkg.Threat_Numeric = cursor.getFloat(3);
+            pkg.ProcessName = cursor.getString(4);
+            pkg.Uid = cursor.getString(5);
+            pkg.ID = cursor.getInt(6);
+
+            pkgList.add(pkg);
+        }
+
+        cursor.close();
+
+        return pkgList;
+    }
+
+    // returns firts package with specified name
+    public APackage getPackage(String name) {
+        SQLiteDatabase aidsDB = this.getReadableDatabase();
+
+        SQLiteCursor cursor = (SQLiteCursor) aidsDB.query(PACKAGE_TABLE_NAME,
+                new String[] { PACKAGE_NAME, PACKAGE_INSTALL_TIME,
+                        PACKAGE_UPDATE_TIME, PACKAGE_NUMERIC_THREAT,
+                        PROCESS_NAME, PROCESS_UID, ID, PACKAGE_THREAT },
+                PACKAGE_NAME + "=? or " + PROCESS_NAME + "=?",
+                new String[] { name }, null, null, TIMESTAMP + " DESC");
+
+        if (cursor.getCount() == 0) {
+            return null;
+        }
+
+        // although shouldn't be more than one, only care about the first just
+        // in case
+        cursor.moveToFirst();
+
+        APackage pkg = new APackage();
+
+        pkg.Name = cursor.getString(0);
+        pkg.InstallTime = cursor.getLong(1);
+        pkg.UpdateTime = cursor.getLong(2);
+        pkg.Threat_Numeric = cursor.getFloat(3);
+        pkg.ProcessName = cursor.getString(4);
+        pkg.Uid = cursor.getString(5);
+        pkg.ID = cursor.getInt(6);
+        pkg.Threat = APackage.Threat_Type.values()[cursor.getInt(7)];
+        cursor.close();
+
+        return pkg;
+    }
+
+    public boolean updatePackage(APackage pkg) {
+        SQLiteDatabase aidsDB = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(PACKAGE_THREAT, pkg.Threat.ordinal());
+        values.put(PACKAGE_NUMERIC_THREAT, pkg.Threat_Numeric);
+
+        int affectedRows = aidsDB.update(PACKAGE_TABLE_NAME, values, ID + "=?",
+                new String[] { String.valueOf(pkg.ID) });
+
+        if (affectedRows != 1) {
+            return false;
+        }
+
+        return true;
     }
 
 
